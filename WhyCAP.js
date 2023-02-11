7097c5dd06a40df877d87b9411de853830a9b592
UrmilaSharma1 <111847834+UrmilaSharma1@users.noreply.github.com>
2023-02-11 19:44:07 +0530

    initd css

---
+++ WhyCAP.js
@@ -0,0 +1,187 @@
+import React from 'react'
+import './Cap.css'
+const WhyCAP = () => {
+  return (
+<section id="services" className="services section-show __web-inspector-hide-shortcut__">
+    <div className="container">
+
+      <div className="section-title whysp">
+
+          <p className='text-center'>What’s in it for you?</p>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch ">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-user-plus"></i></div>
+            <h4>Part of the NSS</h4>
+            <p>Get a chance to be a part of the NSS IITD community</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-star"></i></div>
+            <h4>Interconnectivity</h4>
+            <p>Better interconnectivity between IITD and the rest of the Delhi students</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-calendar-check"></i></div>
+            <h4>Organize events</h4>
+            <p>Chance to organize events and workshops in collaboration with NSS in your college.</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-receipt"></i></div>
+            <h4>Get Certified</h4>
+            <p>Get Certified by NSS and IITD on completion of your tenure. Certificates would be given to only those CAs
+              who complete the internship.</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-clipboard-user"></i></div>
+            <h4>Visit IIT Delhi</h4>
+            <p>Get a chance to explore one of the biggest Social Fest in India, Kaizen.Access to events at IITD.</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-brands fa-guilded"></i></div>
+            <h4>Mentored by IIT students</h4>
+            <p>Getting mentored by the NSS team and the top minded IIT students.</p>
+          </div>
+        </div>
+
+      </div>
+      <a name="learningop" className="whyanchor"></a>
+      <div className="section-title mt-5">
+        <h2 className="whyh2">Learning opportunities</h2>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-brands fa-nfc-symbol"></i></div>
+            <h4>Networking Opportunities</h4>
+            <p>Networking Opportunities with Industry Experts and experienced professionals.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-book-open-reader"></i></div>
+            <h4>Leadership and Marketing skills</h4>
+            <p>Enhance your leadership and marketing skills and also be able to spread the Entrepreneurship culture on
+              the campus.</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-certificate"></i></div>
+            <h4>Online branding</h4>
+            <p>You will learn social media growth hacks, online branding, and promotion strategies.</p>
+          </div>
+        </div>
+
+      </div>
+      <a name="perks" className="whyanchor"></a>
+      <div className="section-title mt-5">
+        <h2 className="whyh2">Perks for top performers</h2>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-shield"></i></div>
+            <h4>IIT Delhi events</h4>
+            <p>Access to events at IIT Delhi.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i className="fa-solid fa-award"></i></div>
+            <h4>Letter of Recommendation</h4>
+            <p>Top Performers get a Letter of Recommendation.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-user"></i></div>
+            <h4>Internship Opportunities</h4>
+            <p>Top Performers get Internship Opportunities and Exclusive Merchandise.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+      <a name="responsibilities" className="whyanchor"></a>
+      <div className="section-title mt-5">
+        <h2 className="whyh2">Responsibilities</h2>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-atom"></i></div>
+            <h4>Outreach</h4>
+            <p>Increase the outreach of NSS, IIT Delhi's
+              initiatives to promote social activities
+              in your college network.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-comments"></i></div>
+            <h4>Communication</h4>
+            <p>Being a single point of contact
+              for all communication
+              between NSS and your
+              respective colleges.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-lightbulb"></i></div>
+            <h4>Creative Ideas</h4>
+            <p>Developing campus-specific social campaigns and promoting social service are some of the key areas Kaizen focuses on.</p>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 cap-cards">
+          <div className="icon-box w-100  cap-cards">
+            <div className="icon"><i class="fa-solid fa-gear"></i></div>
+            <h4>Orchestrate
+            </h4>
+            <p>Planning &amp; organizing social-related
+              events in your college with the support of the eDC, IIT Delhi.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+  )
+}
+
+export default WhyCAP
